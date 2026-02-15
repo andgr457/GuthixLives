e3aa44f19e748ed8ea99718c67513ac69ca03a8a
Andrew Grinder <andgr457@gmail.com>
2026-02-15 15:44:55 -0600

    added home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import GETracker from './pages/GETracker';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/getracker" />} />
+      <Route path="/" element={<Home />} />
       <Route path="/getracker" element={<GETracker />} />
     </Routes>
   );
